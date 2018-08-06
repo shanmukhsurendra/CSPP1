ee91252e0242fba995a2d21f3e88e2c26d4f4120
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-08-06 22:47:17 +0530

    " -> 10 of 10 passed. pylint: 1.60/10 "

--- cspp1-assignments/m7/p3 (3)/Functions - Assignment-3/assignment3.py
+++ cspp1-assignments/m7/p3 (3)/Functions - Assignment-3/assignment3.py
@@ -38,24 +38,24 @@
 
 
 def payingdebtoffinayear(balance_in, annual_interestrate):
-	init_balance = balance_in
-	moninterest_rate = annual_interestrate/12
-	low_i = init_balance/12
-	up_i = (init_balance* (1+ moninterest_rate)**12)/12.0
-	epsilon = 0.03
-	while abs(balance_in) > epsilon:
-		mon_payrate = (up_i+low_i)/2
-		balance_in = init_balance
-		for _ in range(12):
-			ans_i = balance_in - mon_payrate
-			balance_in = ans_i + (ans_i * moninterest_rate)
-		if balance_in > epsilon:
-			low_i = mon_payrate
-		elif balance_in < -epsilon:
-			up_i = mon_payrate
-		else:
-			break
-	return str(round(mon_payrate, 2))
+    init_balance = balance_in
+    moninterest_rate = annual_interestrate/12
+    low_i = init_balance/12
+    up_i = (init_balance* (1+ moninterest_rate)**12)/12.0
+    epsilon = 0.03
+    while abs(balance_in) > epsilon:
+        mon_payrate = (up_i+low_i)/2
+        balance_in = init_balance
+        for _ in range(12):
+            ans_i = balance_in - mon_payrate
+            balance_in = ans_i + (ans_i * moninterest_rate)
+        if balance_in > epsilon:
+            low_i = mon_payrate
+        elif balance_in < -epsilon:
+            up_i = mon_payrate
+        else:
+            break
+    return str(round(mon_payrate, 2))
 
 
 def main():
@@ -63,7 +63,7 @@ def main():
     # data = "4773 0.2"
     data = data.split(' ')
     data = list(map(float, data))
-    print("Lowest payment:",(payingdebtoffinayear(data[0],data[1])))
+    print("Lowest Payment:",(payingdebtoffinayear(data[0],data[1])))
     
 if __name__ == "__main__":
     main()
