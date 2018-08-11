59180cb67ed6762093272dea97efbe595b4840dd
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-08-11 10:52:10 +0530

    " -> 0 of 3 passed. pylint: 8.24/10 "

--- cspp1-assignments/m12/p1/p1/create_social_network.py
+++ cspp1-assignments/m12/p1/p1/create_social_network.py
@@ -36,7 +36,12 @@ def create_social_network(string):
     d = {}
     l = []
     l = string.split("follows")
-    return l
+    for i in range(0, len(l), 2):
+        if l[i] not in d:
+            d[l[i]] = l[i+1].split(",")
+    return d
+
+
 
 def main():
     '''
