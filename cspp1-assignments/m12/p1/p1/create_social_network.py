199db4c93c4de2ef050c286be4606ed7188e7f2a
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-08-11 16:52:12 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- cspp1-assignments/m12/p1/p1/create_social_network.py
+++ cspp1-assignments/m12/p1/p1/create_social_network.py
@@ -35,12 +35,10 @@ def create_social_network(string):
     # remove the pass below and start writing your code
     dictn_n = {}
     list_l = []
-    list_l = string.split("follows")
+    list_l = string.split()
     print(list_l)
-    if "follows" not in string:
-        return dictn_n
-    else:
-        for i in range(0, len(list_l)-1, 2):
+    for i in range(0, len(list_l)-1, 2):
+    	if list_l[i+1] == 'follows'
             if list_l[i] not in dictn_n:
                 dictn_n[list_l[i]] = list_l[i+1].split(",")
         print(dictn_n)
