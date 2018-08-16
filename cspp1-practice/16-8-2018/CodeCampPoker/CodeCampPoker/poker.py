2fc1117e7fb379dc7b121663b6133e9ce03481c1
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-08-16 22:53:40 +0530

    " -> 0 of 19 passed. pylint: 9.15/10 "

--- cspp1-practice/16-8-2018/CodeCampPoker/CodeCampPoker/poker.py
+++ cspp1-practice/16-8-2018/CodeCampPoker/CodeCampPoker/poker.py
@@ -170,8 +170,8 @@ def hand_rank(hand):
         for i in range(len(card_rank)-1):
             if card_rank[i] == card_rank[i+1]:
                 tem_p = card_rank[i]
-                card_rank = []
-                card_rank.append(tem_p)
+                card_rank_k = []
+                card_rank_k.append(tem_p)
                 break
         card_value = 2
     elif high_card(hand):
