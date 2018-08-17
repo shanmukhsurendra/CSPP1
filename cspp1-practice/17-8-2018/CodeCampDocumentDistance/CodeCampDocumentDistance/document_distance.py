4adba893cd23a3437200712353978a86fa3d2612
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-08-17 12:32:42 +0530

    " -> 0 of 3 passed. pylint: 3.80/10 "

--- cspp1-practice/17-8-2018/CodeCampDocumentDistance/CodeCampDocumentDistance/document_distance.py
+++ cspp1-practice/17-8-2018/CodeCampDocumentDistance/CodeCampDocumentDistance/document_distance.py
@@ -2,16 +2,51 @@
     Document Distance - A detailed description is given in the PDF
 '''
 import re
+import math
+def get_frequency(lis_t, lis2_t):
+	freq_dict = {}
+	for i in lis_t:
+		if i in freq_dict:
+			freq_dict[i][0] += 1
+		else:
+			freq_dict[i] = [1]
+		freq_dict[i].append(0)
+	for i in lis2_t:
+		if i in freq_dict:
+			freq_dict[i][1] += 1
+		else:
+			freq_dict[i] = [0, 1]
+	return freq_dict
+
+
 def similarity(dict1, dict2):
     '''
         Compute the document distance as given in the PDF
     '''
     stop_words = {}
-    input1_dict = ((re.sub(r'[^\w\s]','', dict1)).lower()).split()
-    input2_dict = ((re.sub(r'[^\w\s]','', dict2)).lower()).split()
-    #print(input1_dict)
+    lis_t = []
+    lis2_t = []
+    input1_list = ((re.sub(r'[^\w\s]','', dict1)).lower()).split()
+    input2_list = ((re.sub(r'[^\w\s]','', dict2)).lower()).split()
+    #print(input1_list)
     stop_words = load_stopwords("stopwords.txt")
     print(stop_words)
+    for i in input1_list:
+    	if i not in stop_words:
+    		lis_t.append(i)
+    for i in input2_list:
+    	if i not in stop_words:
+    		lis2_t.append(i)
+    word_freq = get_frequency(lis_t, lis2_t)
+    number = 0
+    d_1d = 0
+    d_2d = 0
+    for k in word_freq:
+    	number += word_freq[k][0]*word_freq[k][1]
+    	d_1d += word_freq[k][0]**2
+    	d_2d += word_freq[k][1]**2
+    	return abs((number/ (math.sqrt(d_1d)* math.d_2d.sqrt(d_2d))))
+
 def load_stopwords(filename):
     '''
         loads stop words from a file and returns a dictionary
