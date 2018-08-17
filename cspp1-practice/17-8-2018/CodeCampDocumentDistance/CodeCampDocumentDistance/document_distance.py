ca7377b1e2a75cdab1f48866fefb8fa2f3509369
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-08-17 12:33:23 +0530

    " -> 1 of 3 passed. pylint: 4.69/10 "

--- cspp1-practice/17-8-2018/CodeCampDocumentDistance/CodeCampDocumentDistance/document_distance.py
+++ cspp1-practice/17-8-2018/CodeCampDocumentDistance/CodeCampDocumentDistance/document_distance.py
@@ -45,7 +45,7 @@ def similarity(dict1, dict2):
     	number += word_freq[k][0]*word_freq[k][1]
     	d_1d += word_freq[k][0]**2
     	d_2d += word_freq[k][1]**2
-    	return abs((number/ (math.sqrt(d_1d)* math.d_2d.sqrt(d_2d))))
+    	return abs((number/ (math.sqrt(d_1d)* math.sqrt(d_2d))))
 
 def load_stopwords(filename):
     '''
