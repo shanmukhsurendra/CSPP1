22319aedaa148a6531ea491123a5863f8fecd810
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-08-18 09:41:47 +0530

    " -> 0 of 1 passed. pylint: 7.60/10 "

--- cspp1-assignments/m18/p1/p1/build_search_index.py
+++ cspp1-assignments/m18/p1/p1/build_search_index.py
@@ -39,7 +39,7 @@ def word_list(text):
         Clean up the text by remvoing all the non alphabet characters
         return a list of words
     '''
-    print(text)
+    #print(text)
     # stop_words = {}
     # lis_t = []
     # lis2_t = []
