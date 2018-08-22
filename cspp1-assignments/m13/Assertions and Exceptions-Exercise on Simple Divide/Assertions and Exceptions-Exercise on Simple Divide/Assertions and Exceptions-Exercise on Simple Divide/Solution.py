e6b5b29e60a251c4e745d2a505e6bd5f3fdc5372
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-08-22 18:31:08 +0530

    " -> 0 of 5 passed. pylint: 4.63/10 "

---
+++ cspp1-assignments/m13/Assertions and Exceptions-Exercise on Simple Divide/Assertions and Exceptions-Exercise on Simple Divide/Assertions and Exceptions-Exercise on Simple Divide/Solution.py
@@ -0,0 +1,31 @@
+#define the simple_divide function here
+def simple_divide(item, denom):
+    try:
+        item / denom
+    except ZeroDivisionError:
+        return 0
+    else:
+        return item / denom
+
+
+    # start a try-except block
+   
+def fancy_divide(list_of_numbers, index):
+    denom = list_of_numbers[index]
+    return [simple_divide(item, denom) for item in list_of_numbers]
+
+    
+
+def main():
+    data = raw_input()
+    l=data.split()
+    l1=[]
+    for j in l:
+        l1.append(float(j))
+    s=raw_input()
+    index=int(s)
+    print fancy_divide(l1,index)
+    
+
+if __name__== "__main__":
+    main()
