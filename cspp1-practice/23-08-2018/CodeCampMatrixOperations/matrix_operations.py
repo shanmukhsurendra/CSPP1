f6e2ac820dbdafa8e3ea1c725e7613061df50564
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-08-23 17:38:44 +0530

    " -> 1 of 8 passed. pylint: 6.18/10 "

--- cspp1-practice/23-08-2018/CodeCampMatrixOperations/matrix_operations.py
+++ cspp1-practice/23-08-2018/CodeCampMatrixOperations/matrix_operations.py
@@ -12,15 +12,17 @@ def mult_matrix(m1, m2):
     #         for j in range(len(m2)):
     #             a += m1[i][j]*m2[j][i]
     #         l_new.append(a)
-    grid = [[0]*len(m1)]*len(m2[0])
-    if len(m1[0]) == len(m2):
-        for i in range(len(m1)):
-            for j in range(len(m2[0])):
-                for k in range(len(m2)):
-                    grid[i][j] += m1[i][k] * m2[k][j]
-        return grid
-    else:
+    if len(m1[0]) != len(m2[0]):
         print("Error: Matrix shapes invalid for mult")
+        return
+    grid = [[0]*len(m1)]*len(m2[0])
+    for i in range(len(m1)):
+        for j in range(len(m2[0])):
+            for k in range(len(m2)):
+                grid[i][j] += int(m1[i][k]) * int(m2[k][j])
+    return grid
+
+        
 
 def add_matrix(m1, m2):
     '''
