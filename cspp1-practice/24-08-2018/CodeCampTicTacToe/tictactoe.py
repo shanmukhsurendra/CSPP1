fb32849345811b3c5919d3cb13043ee5bb193b33
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-08-24 14:21:16 +0530

    " -> 14 of 14 passed. pylint: 9.13/10 "

--- cspp1-practice/24-08-2018/CodeCampTicTacToe/tictactoe.py
+++ cspp1-practice/24-08-2018/CodeCampTicTacToe/tictactoe.py
@@ -12,14 +12,12 @@ def winn_er(read_m):
         return read_m[1][0]
     elif read_m[2][0] == read_m[2][1] == read_m[2][2]:
         return read_m[2][0]
-    elif read_m[0][2] == read_m[1][2] == read_m[1][2]:
-        return read_m[2][2]
-    elif read_m[2][0] == read_m[1][1] == read_m[0][2]:
-        return read_m[2][0]
+    elif read_m[0][2] == read_m[1][2] == read_m[2][2] or read_m[2][0] == read_m[1][1] == read_m[0][2]:
+        return read_m[0][2]
     elif read_m[0][1] == read_m[1][1] == read_m[2][1]:
         return read_m[0][1]
     else:
-        return None
+        return None 
 def valid_input_game(read_m):
     '''
     to find out the valid input
