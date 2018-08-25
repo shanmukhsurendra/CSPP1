09f3899f8942231d0a315388cb10a7fe1e537484
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-08-25 10:37:05 +0530

    " -> 0 of 2 passed. pylint: 0/0 "

--- cspp1-assignments/m22/assignment1/assignment1/read_input.py
+++ cspp1-assignments/m22/assignment1/assignment1/read_input.py
@@ -4,7 +4,7 @@ Write a python program to read multiple lines of text input and store the input 
 
 def main():
 	a_a = ""
-    n_n = int(input)
+    n_n = int(input())
     for i_i in range(n_n):
     	a_a = input()
     	print(a_a)
